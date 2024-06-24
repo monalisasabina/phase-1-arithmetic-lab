772cf73b093207ad1060551fd8bbf3dbe8187472
Monalisa Onyango <+>
2024-06-24 22:17:42 +0300

    arithmetric lab

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+let num1=31;
+let num2=2;
+
+let multiply= num1 * num2
+console.log(multiply);
+
+
+let random=12
+console.log(Number.isInteger(random));
+
+
+let num3=16
+let num4=6
+
+let mod=num3 %= (num4);
+console.log(mod);
+
+let max=(1,3,5,8,9,13,15,20);
+console.log(Math.max(max));
